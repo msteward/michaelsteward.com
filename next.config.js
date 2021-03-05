0f95865637d218b5ae25f5c2818c4da2750648e2
Michael Steward <michael.steward@lendesk.com>
2021-03-05 09:09:50 -0800

    Add i18n config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+
+    domains: [
+      {
+        domain: 'michaelsteward.com',
+        defaultLocale: 'en-US',
+      },
+    ],
+  },
+};
