913023abdf31b86707157a165bd2e8c3ed742cb1
Michael Steward <michael.steward@lendesk.com>
2021-03-04 21:16:24 -0800

    Convert to nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Michael Steward</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          michael steward
+        </h1>
+      </main>
+    </div>
+  )
+}
